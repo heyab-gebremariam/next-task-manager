71d34995acd82eab6f013ade023a92a05ac086e7
heyab-gebremariam <heyab.gebremariam@gmail.com>
2025-07-25 15:26:33 +0300

    railway postgres

--- prisma/migrations/20250725102708_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "status" TEXT NOT NULL DEFAULT 'pending',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
